694cefd69da2bb3225c186ab5617ae433f1f3ca9
Daniel <znsoft@163.com>
2021-01-05 15:53:55 +0800

    Create convert.sh

---
+++ convert.sh
@@ -0,0 +1,3 @@
+
+ paddle2onnx --model_dir models/ch_ppocr_mobile_v2.0_cls_infer/  --model_filename  inference.pdmodel --params_filename inference.pdiparams --save_file ch_ppocr_mobile_v2.0_cls_infer.onnx --opset_version 10 --enable_onnx_checker True
+ 
