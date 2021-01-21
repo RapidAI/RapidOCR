5d45976e98db10d33a0d79012b03a03bf269a376
root <root@A000769-PC.ESG.360ES.CN>
2021-01-21 16:44:09 +0800

    Revert "Update gen-models.sh"
    This reverts commit 2b756e9fa6bf30161c5217c5b0ff85d4af1e81ca.

--- tools/gen-models.sh
+++ tools/gen-models.sh
@@ -10,7 +10,7 @@ python3 tools/export_custom.py --height -1 --width -1 -c configs/det/ch_ppocr_v2
 
 paddle2onnx --model_dir models/ch_ppocr_mobile_v2.0_det_infer/  --model_filename  inference.pdmodel --params_filename inference.pdiparams --save_file models/ch_ppocr_mobile_v2.0_det_infer.onnx --opset_version 11 --enable_onnx_checker True
 
-# ======ch_ppocr_mobile_v2.0_rec======
+# ======ch_ppocr_mobile_v2.0_rec failed======
 python3 tools/export_custom.py --height 32 --width -1 -c configs/rec/ch_ppocr_v2.0/rec_chinese_lite_train_v2.0.yml -o Global.pretrained_model=./models/ch_ppocr_mobile_v2.0_rec_train/best_accuracy Global.load_static_weights=False Global.save_inference_dir=./models/ch_ppocr_mobile_v2.0_rec_infer/
 
 paddle2onnx --model_dir models/ch_ppocr_mobile_v2.0_rec_infer/  --model_filename  inference.pdmodel --params_filename inference.pdiparams --save_file models/ch_ppocr_mobile_v2.0_rec_infer.onnx --opset_version 11 --enable_onnx_checker True
