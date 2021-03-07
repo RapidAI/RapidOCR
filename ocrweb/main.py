3f05a8f31f3f741c651813ce2309a343e5e4ac92
SWHL <liekkaskono@163.com>
2021-03-07 21:03:19 +0800

    添加基于redis和RQ队列后台执行方式web界面

--- ocrweb/main.py
+++ ocrweb/main.py
@@ -3,24 +3,13 @@
 # @File: main.py
 # @Author: Max
 import base64
-import json
-import time
 
 import cv2
 import numpy as np
 from flask import Flask, render_template, request
 
-from resources.bpocr import TextSystem, draw_text_det_res
+from task import detect_recognize
 
-# 实例化模型
-det_model_path = 'resources/models/ch_ppocr_server_v2.0_det_infer.onnx'
-cls_model_path = 'resources/models/ch_ppocr_mobile_v2.0_cls_infer.onnx'
-rec_model_path = 'resources/models/ch_ppocr_mobile_v2.0_rec_infer.onnx'
-
-text_sys = TextSystem(det_model_path,
-                      rec_model_path,
-                      use_angle_cls=True,
-                      cls_model_path=cls_model_path)
 
 app = Flask(__name__)
 
@@ -41,35 +30,7 @@ def ocr():
         image = base64.b64decode(url_get)
         nparr = np.frombuffer(image, np.uint8)
         image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-
-        t1 = time.time()
-        dt_boxes, rec_res, img = text_sys(image)
-        t2 = time.time()
-        if dt_boxes is None or rec_res is None:
-            temp_rec_res = []
-            rec_res_data = json.dumps(temp_rec_res,
-                                      indent=2,
-                                      ensure_ascii=False)
-            elapse = 0
-            image = cv2.imencode('.jpg', img)[1]
-            img = str(base64.b64encode(image))[2:-1]
-        else:
-            temp_rec_res = []
-            for i, value in enumerate(rec_res):
-                temp_rec_res.append([i, value[0], value[1]])
-            temp_rec_res = np.array(temp_rec_res)
-            rec_res_data = json.dumps(temp_rec_res.tolist(),
-                                      indent=2,
-                                      ensure_ascii=False)
-
-            det_im = draw_text_det_res(dt_boxes, img)
-            image = cv2.imencode('.jpg', det_im)[1]
-            img = str(base64.b64encode(image))[2:-1]
-
-            elapse = f'{t2-t1:.3f}'
-        return json.dumps({'image': img,
-                           'elapse': elapse,
-                           'rec_res': rec_res_data})
+        return detect_recognize(image)
 
 
 if __name__ == '__main__':
--- ocrweb/readme.md
+++ ocrweb/readme.md
@@ -1,3 +1,29 @@
-#### The whole project for web based OCR application.
 
-#### TODO
+#### 目录结构
+```text
+.
+├── main.py
+├── main_redis.py
+├── readme.md
+├── resources
+│   ├── bpocr.py
+│   ├── ch_ppocr_mobile_v2_cls
+│   ├── ch_ppocr_mobile_v2_det
+│   ├── ch_ppocr_mobile_v2_rec
+│   ├── __init__.py
+│   ├── models
+│   └── __pycache__
+├── static
+│   ├── css
+│   ├── images
+│   └── js
+├── task.py  # 单独开启rq worker时使用
+└── templates
+    ├── index.html
+    └── index_redis.html  # 带有redis队列web界面
+```
+
+#### Redis队列web界面使用步骤
+- 安装redis,并开启redis服务
+- `rq worker` 启动工作进程，处理实际任务
+- `python main_redis.py` 开启前端接受数据界面
