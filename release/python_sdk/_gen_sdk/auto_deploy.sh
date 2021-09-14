0df53d8515f6b12441a508898876f6a31b1d1bdb
SWHL <liekkaskono@163.com>
2021-09-14 22:54:03 +0800

    Fix bugs of workflow

--- release/python_sdk/_gen_sdk/auto_deploy.sh
+++ release/python_sdk/_gen_sdk/auto_deploy.sh
@@ -1,21 +1,15 @@
 #! /bin/bash
 
 sdk_dir="sdk_rapidocr_v1.0.0"
+mkdir ${sdk_dir}
 
-echo ">>>>>>打包现有的"
 python setup.py bdist_wheel
 mv dist/*.whl ${sdk_dir}/
 rm -r build
 rm -r dist
 rm -r *.egg-info
 
-echo ">>>>>>安装在当前环境下"
-pip install ${sdk_dir}/*.whl
-
-echo ">>>>>>覆盖README.md"
 cp "requirements.txt" "${sdk_dir}/"
-
-echo ">>>>>>测试whl是否有误"
-
-cd ${sdk_dir}
-python test_demo.py
+cp -r resources ${sdk_dir}/
+cp -r images ${sdk_dir}/
+cp test_demo.py ${sdk_dir}/
