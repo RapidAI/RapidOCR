1c27ff0b5bc2ab968e3b4bc99deb7f5569cf6009
SWHL <liekkaskono@163.com>
2021-10-11 22:23:43 +0800

    Clean python_sdk rapidocr

--- release/python_sdk/_gen_sdk/auto_deploy.sh
+++ release/python_sdk/_gen_sdk/auto_deploy.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
 sdk_dir="sdk_rapidocr_v1.0.0"
-if [ -d "${sdk_dir}" ]; then
+if [ -d "../${sdk_dir}" ]; then
     echo "存在，删除掉"
-    rm -r ${sdk_dir}
+    rm -r "../${sdk_dir}"
 fi
 mkdir ${sdk_dir}
 
