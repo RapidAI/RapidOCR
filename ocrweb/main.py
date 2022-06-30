9b322bcbe65cea74c8a1872c04926a9fee0a5440
SWHL <liekkaskono@163.com>
2022-06-30 13:44:01 +0800

    Update ocrweb ip

--- ocrweb/main.py
+++ ocrweb/main.py
@@ -35,4 +35,4 @@ def ocr():
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=9003, debug=False, processes=True)
+    app.run(host='0.0.0.0', port=9003, debug=False, processes=True)
