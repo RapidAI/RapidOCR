f45c5a1385211fd9f4282468213899b6f23f6e25
SWHL <liekkaskono@163.com>
2022-08-14 22:43:40 +0800

    Unified name and update issue #42

--- python/tests/test_det.py
+++ python/tests/test_det.py
@@ -5,14 +5,16 @@ import base
 import cv2
 
 
-module_name = 'ch_ppocr_v2_det'
+module_name = 'ch_ppocr_v3_det'
 class_name = 'TextDetector'
 TextDetector = base.init_module(module_name, class_name)
 
 yaml_path = base.root_dir / base.package_name / module_name / 'config.yaml'
+print(yaml_path)
 config = base.read_yaml(str(yaml_path))
 config['model_path'] = str(base.root_dir / config['model_path'])
 
+print(config)
 text_det = TextDetector(config)
 
 
