48036cffe3870928a6a2e35f12cbd0c98706c653
Haoran Jia <catty97@163.com>
2022-12-10 14:22:00 +0800

    本方法实现了确认图片是否为真图片的功能。在task.py里写了个check_image_type的函数，并在main里调用

--- ocrweb/main.py
+++ ocrweb/main.py
@@ -10,6 +10,7 @@ import numpy as np
 from flask import Flask, render_template, request
 
 from task import detect_recognize
+from task import check_image_type
 
 app = Flask(__name__)
 app.config['MAX_CONTENT_LENGTH'] = 3 * 1024 * 1024
@@ -29,14 +30,21 @@ def ocr():
         image = base64.b64decode(img_str + '=' * (-len(img_str) % 4))
         nparr = np.frombuffer(image, np.uint8)
         image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-        if image.ndim == 2:
-            image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
+        # 验证image是不是一张图片
+        # Provide by BUPT
+        check = check_image_type(image)
+        print(check)
+        if check:
+            if image.ndim == 2:
+                image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
 
-        img, elapse, elapse_part, rec_res_data = detect_recognize(image)
-        return json.dumps({'image': img,
-                           'total_elapse': f'{elapse:.4f}',
-                           'elapse_part': elapse_part,
-                           'rec_res': rec_res_data})
+            img, elapse, elapse_part, rec_res_data = detect_recognize(image)
+            return json.dumps({'image': img,
+                            'total_elapse': f'{elapse:.4f}',
+                            'elapse_part': elapse_part,
+                            'rec_res': rec_res_data})
+        else:
+            return json.dumps({'message':'请选择正确的图片格式'})
 
 
 if __name__ == '__main__':
