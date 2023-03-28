d80b723960a8e15876a75de823d8603e092a5e2a
SWHL <liekkaskono@163.com>
2023-03-28 19:09:44 +0800

    Optim setup func

--- python/setup_openvino.py
+++ python/setup_openvino.py
@@ -1,31 +1,11 @@
 # -*- encoding: utf-8 -*-
 # @Author: SWHL
 # @Contact: liekkaskono@163.com
-import re
-import subprocess
 import sys
 from pathlib import Path
 
 import setuptools
-
-
-def get_latest_version(package_name):
-    output = subprocess.run(["pip", "index", "versions", package_name],
-                            capture_output=True)
-    output = output.stdout.decode('utf-8')
-    if output:
-        return extract_version(output)
-    return None
-
-
-def version_add_one(version, add_loc=-1):
-    if version:
-        version_list = version.split('.')
-        mini_version = str(int(version_list[add_loc]) + 1)
-        version_list[add_loc] = mini_version
-        new_version = '.'.join(version_list)
-        return new_version
-    return '0.0.0'
+from get_pypi_latest_version import GetPyPiLatestVersion
 
 
 def get_readme():
@@ -37,22 +17,16 @@ def get_readme():
     return readme
 
 
-def extract_version(message: str) -> str:
-    pattern = r'\d+\.(?:\d+\.)*\d+'
-    matched_versions = re.findall(pattern, message)
-    if matched_versions:
-        return matched_versions[0]
-    return ''
-
-
 MODULE_NAME = 'rapidocr_openvino'
-latest_version = get_latest_version(MODULE_NAME)
-VERSION_NUM = version_add_one(latest_version)
+
+obtainer = GetPyPiLatestVersion()
+latest_version = obtainer(MODULE_NAME)
+VERSION_NUM = obtainer.version_add_one(latest_version)
 
 # 优先提取commit message中的语义化版本号，如无，则自动加1
 if len(sys.argv) > 2:
     match_str = ' '.join(sys.argv[2:])
-    matched_versions = extract_version(match_str)
+    matched_versions = obtainer.extract_version(match_str)
     if matched_versions:
         VERSION_NUM = matched_versions
 sys.argv = sys.argv[:2]
@@ -85,6 +59,7 @@ setuptools.setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
     ],
+    python_requires='>=3.6,<=3.10',
     entry_points={
         'console_scripts': [f'{MODULE_NAME}={MODULE_NAME}.rapid_ocr_api:main'],
     }
