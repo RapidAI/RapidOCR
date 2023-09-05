50ce26671ab4a78955f8bc773348f82ad9fe0024
SWHL <liekkaskono@163.com>
2023-09-05 22:17:40 +0800

    Remove invalid function

--- python/rapidocr_openvino/ch_ppocr_v3_det/utils.py
+++ python/rapidocr_openvino/ch_ppocr_v3_det/utils.py
@@ -21,43 +21,7 @@ import sys
 import cv2
 import numpy as np
 import pyclipper
-import six
 from shapely.geometry import Polygon
-
-
-class DecodeImage:
-    """decode image"""
-
-    def __init__(self, img_mode="RGB", channel_first=False):
-        self.img_mode = img_mode
-        self.channel_first = channel_first
-
-    def __call__(self, data):
-        img = data["image"]
-        if six.PY2:
-            assert (
-                type(img) is str and len(img) > 0
-            ), "invalid input 'img' in DecodeImage"
-        else:
-            assert (
-                type(img) is bytes and len(img) > 0
-            ), "invalid input 'img' in DecodeImage"
-
-        img = np.frombuffer(img, dtype="uint8")
-        img = cv2.imdecode(img, 1)
-        if img is None:
-            return None
-
-        if self.img_mode == "GRAY":
-            img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
-        elif self.img_mode == "RGB":
-            assert img.shape[2] == 3, f"invalid shape of image[{img.shape}]"
-            img = img[:, :, ::-1]
-
-        if self.channel_first:
-            img = img.transpose((2, 0, 1))
-        data["image"] = img
-        return data
 
 
 class NormalizeImage:
