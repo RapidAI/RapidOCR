cb14cc846ba51d28ffe9f978aa726a43a2d0193f
SWHL <liekkaskono@163.com>
2024-05-21 08:06:25 +0800

    style(rapidocr_onnxruntime): Code wrapping with black

--- python/rapidocr_onnxruntime/utils/parse_parameters.py
+++ python/rapidocr_onnxruntime/utils/parse_parameters.py
@@ -119,7 +119,12 @@ class UpdateParameters:
         global_dict, det_dict, cls_dict, rec_dict = self.parse_kwargs(**kwargs)
         new_config = {
             "Global": self.update_global_params(config["Global"], global_dict),
-            "Det": self.update_params(config["Det"], det_dict, "det_", ["det_model_path", "det_use_cuda", "det_use_dml"]),
+            "Det": self.update_params(
+                config["Det"],
+                det_dict,
+                "det_",
+                ["det_model_path", "det_use_cuda", "det_use_dml"],
+            ),
             "Cls": self.update_params(
                 config["Cls"],
                 cls_dict,
@@ -127,7 +132,10 @@ class UpdateParameters:
                 ["cls_label_list", "cls_model_path", "cls_use_cuda", "cls_use_dml"],
             ),
             "Rec": self.update_params(
-                config["Rec"], rec_dict, "rec_", ["rec_model_path", "rec_use_cuda", "rec_use_dml"]
+                config["Rec"],
+                rec_dict,
+                "rec_",
+                ["rec_model_path", "rec_use_cuda", "rec_use_dml"],
             ),
         }
 
