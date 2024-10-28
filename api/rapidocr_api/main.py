3aa4463ad20bc9dc8d8b08766d0f46d7699efc57
xmxoxo <xmxoxo@users.noreply.github.com>
2024-10-28 08:12:23 +0800

    update rapidocr_api (#242)
    Co-authored-by: 何熙 <hexi@1633.com>

--- api/rapidocr_api/main.py
+++ api/rapidocr_api/main.py
@@ -1,9 +1,11 @@
 # -*- encoding: utf-8 -*-
 # @Author: SWHL
 # @Contact: liekkaskono@163.com
+
 import argparse
 import base64
 import io
+import os
 import sys
 from pathlib import Path
 from typing import Dict
@@ -19,37 +21,47 @@ sys.path.append(str(Path(__file__).resolve().parent.parent))
 
 class OCRAPIUtils:
     def __init__(self) -> None:
-        self.ocr = RapidOCR()
+        # 从环境变量中读取参数
+        det_model_path = os.getenv("det_model_path", None)
+        cls_model_path = os.getenv("cls_model_path", None)
+        rec_model_path = os.getenv("rec_model_path", None)
 
-    def __call__(self, img: Image.Image) -> Dict:
+        self.ocr = RapidOCR(det_model_path=det_model_path, cls_model_path=cls_model_path, rec_model_path=rec_model_path)
+
+    def __call__(self, img: Image.Image, use_det=None, use_cls=None, use_rec=None) -> Dict:
         img = np.array(img)
-        ocr_res, _ = self.ocr(img)
+        ocr_res, _ = self.ocr(img, use_det=use_det, use_cls=use_cls, use_rec=use_rec)
 
         if not ocr_res:
             return {}
 
-        out_dict = {
-            str(i): {
-                "rec_txt": rec,
-                "dt_boxes": dt_box,
-                "score": f"{score:.4f}",
-            }
-            for i, (dt_box, rec, score) in enumerate(ocr_res)
-        }
+        # 转换为字典格式: 兼容所有参数情况
+        out_dict = {}
+        for i, dats in enumerate(ocr_res):
+            values = {}
+            for dat in dats:
+                if type(dat) == str:
+                    values["rec_txt"] = dat
+                if type(dat) == np.float64:
+                    values["score"] = f"{dat:.4f}"
+                if type(dat) == list:
+                    values["dt_boxes"] = dat
+            out_dict[str(i)] = values
+
         return out_dict
 
 
 app = FastAPI()
 processor = OCRAPIUtils()
 
-
 @app.get("/")
 async def root():
     return {"message": "Welcome to RapidOCR API Server!"}
 
-
 @app.post("/ocr")
-async def ocr(image_file: UploadFile = None, image_data: str = Form(None)):
+async def ocr(image_file: UploadFile = None, image_data: str = Form(None),
+        use_det: bool = Form(None), use_cls: bool = Form(None), use_rec: bool = Form(None)):
+
     if image_file:
         img = Image.open(image_file.file)
     elif image_data:
@@ -60,19 +72,17 @@ async def ocr(image_file: UploadFile = None, image_data: str = Form(None)):
         raise ValueError(
             "When sending a post request, data or files must have a value."
         )
-
-    ocr_res = processor(img)
+    ocr_res = processor(img, use_det=use_det, use_cls=use_cls, use_rec=use_rec)
     return ocr_res
-
 
 def main():
     parser = argparse.ArgumentParser("rapidocr_api")
     parser.add_argument("-ip", "--ip", type=str, default="0.0.0.0", help="IP Address")
     parser.add_argument("-p", "--port", type=int, default=9003, help="IP port")
+    parser.add_argument('-workers', "--workers", type=int, default=1, help='number of worker process')
     args = parser.parse_args()
 
-    uvicorn.run("rapidocr_api.main:app", host=args.ip, port=args.port, reload=True)
-
+    uvicorn.run("rapidocr_api.main:app", host=args.ip, port=args.port, reload=0, workers=args.workers)
 
 if __name__ == "__main__":
     main()
