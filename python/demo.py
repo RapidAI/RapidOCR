12c85ab6f3287c215c5ba2fc9c0a46671f70882c
Joker1212 <519548295@qq.com>
2024-11-11 16:53:33 +0800

    chore: rm unnecessary change

--- python/demo.py
+++ python/demo.py
@@ -8,31 +8,20 @@ from rapidocr_onnxruntime import RapidOCR, VisRes
 # from rapidocr_paddle import RapidOCR, VisRes
 # from rapidocr_openvino import RapidOCR, VisRes
 
-if __name__ == '__main__':
 
-    engine = RapidOCR()
-    vis = VisRes(0.4)
+engine = RapidOCR()
+vis = VisRes()
 
-    image_path = "tests/test_files/bad.png"
-    with open(image_path, "rb") as f:
-        img = f.read()
+image_path = "tests/test_files/black_font_color_transparent.png"
+with open(image_path, "rb") as f:
+    img = f.read()
 
-    result, elapse_list = engine(img, rec_word_box=True, text_score=0.4)
-    word_result = []
-    for res in result:
-        score = res[2]
-        for word,word_box in zip(res[3], res[4]):
-            word_res = []
-            word_res.append(word_box)
-            word_res.append(word)
-            word_res.append(score)
-            word_result.append(word_res)
-    # print(result)
-    print(elapse_list)
+result, elapse_list = engine(img)
+print(result)
+print(elapse_list)
 
-    # boxes, txts, scores = list(zip(*result))
-    boxes, txts, scores = list(zip(*word_result))
+boxes, txts, scores = list(zip(*result))
 
-    font_path = "resources/fonts/FZYTK.TTF"
-    vis_img = vis(img, boxes, txts, scores, font_path)
-    cv2.imwrite("vis.png", vis_img)
+font_path = "resources/fonts/FZYTK.TTF"
+vis_img = vis(img, boxes, txts, scores, font_path)
+cv2.imwrite("vis.png", vis_img)
