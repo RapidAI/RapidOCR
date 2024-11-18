b11371b94cea3e17291771677b71c46afefe846e
SWHL <liekkaskono@163.com>
2024-11-18 22:52:41 +0800

    chore: update demo

--- python/demo.py
+++ python/demo.py
@@ -2,6 +2,7 @@
 # @Author: SWHL
 # @Contact: liekkaskono@163.com
 import cv2
+import numpy as np
 
 from rapidocr_onnxruntime import RapidOCR, VisRes
 
@@ -16,12 +17,18 @@ image_path = "tests/test_files/ch_en_num.jpg"
 with open(image_path, "rb") as f:
     img = f.read()
 
-result, elapse_list = engine(img, rec_word_box=True)
+result, elapse_list = engine(img, return_word_box=True)
+# result, elapse_list = engine(img)
 print(result)
 print(elapse_list)
 
-boxes, txts, scores = list(zip(*result))
+(boxes, txts, scores, words, words_boxes) = list(zip(*result))
 
 font_path = "resources/fonts/FZYTK.TTF"
 vis_img = vis(img, boxes, txts, scores, font_path)
 cv2.imwrite("vis.png", vis_img)
+
+words_boxes = np.array(words_boxes)
+word_scores = [1.0] * len(words[0])
+vis_img = vis(img, words_boxes[0], words[0], word_scores, font_path)
+cv2.imwrite("vis_single.png", vis_img)
