0ab091c1a0f82199e3c238524dc7c351a28db1bd
SWHL <liekkaskono@163.com>
2025-05-13 20:31:57 +0800

    chore: update demo code

--- python/demo.py
+++ python/demo.py
@@ -1,21 +1,15 @@
 # -*- encoding: utf-8 -*-
 # @Author: SWHL
 # @Contact: liekkaskono@163.com
-from pathlib import Path
-
+# -*- encoding: utf-8 -*-
+# @Author: SWHL
+# @Contact: liekkaskono@163.com
 from rapidocr import RapidOCR
 
-engine = RapidOCR()
+engine = RapidOCR(params={"Global.with_torch": True})
 
-# img_url = "https://img1.baidu.com/it/u=3619974146,1266987475&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=516"
+img_url = "https://img1.baidu.com/it/u=3619974146,1266987475&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=516"
+result = engine(img_url)
+print(result)
 
-# img_paths = ["tmp/en.jpg", "tmp/ch.jpg", "tmp/ch_en.jpg"]
-
-# for img_path in img_paths:
-#     result = engine(img_path, return_word_box=True)
-#     # print(result)
-
-#     result.vis(f"tmp/vis_{Path(img_path).stem}.jpg")
-img_path = "tmp/ch.jpg"
-result = engine(img_path, return_word_box=True)
-result.vis(f"tmp/vis_{Path(img_path).stem}.jpg")
+result.vis("vis_result.jpg")
