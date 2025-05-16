52c011fce280912226f867fba81fd8ec3a9968f6
SWHL <liekkaskono@163.com>
2025-05-16 09:25:22 +0800

    chore: update files

--- python/demo.py
+++ python/demo.py
@@ -1,29 +1,12 @@
 # -*- encoding: utf-8 -*-
 # @Author: SWHL
 # @Contact: liekkaskono@163.com
-# -*- encoding: utf-8 -*-
-# @Author: SWHL
-# @Contact: liekkaskono@163.com
 from rapidocr import RapidOCR
 
-# model_dir = "/Users/joshuawang/projects/_self/tmp/RapidOCR/paddle/PP-OCRv4/rec/ch_doc_PP-OCRv4_rec_server_infer"
-# engine = RapidOCR(
-#     params={
-#         "Global.lang_rec": "ch_doc_server",
-#         "Global.with_paddle": True,
-#         "Rec.model_dir": model_dir,
-#     }
-# )
+engine = RapidOCR(params={"Global.with_torch": True})
 
-engine = RapidOCR(
-    params={"Global.lang_rec": "ch_doc_server", "Global.with_paddle": True}
-)
-
-# img_url = "https://img1.baidu.com/it/u=3619974146,1266987475&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=516"
-# result = engine(img_url)
-
-img_path = "tests/test_files/ch_doc_server.png"
-result = engine(img_path)
+img_url = "https://img1.baidu.com/it/u=3619974146,1266987475&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=516"
+result = engine(img_url)
 print(result)
 
 result.vis("vis_result.jpg")
