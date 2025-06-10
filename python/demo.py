f288b160145e9cbda3c42c83dad57f94985560db
SWHL <liekkaskono@163.com>
2025-06-10 15:10:23 +0800

    chore: update files

--- python/demo.py
+++ python/demo.py
@@ -1,15 +1,9 @@
 # -*- encoding: utf-8 -*-
 # @Author: SWHL
 # @Contact: liekkaskono@163.com
-from rapidocr import EngineType, ModelType, OCRVersion, RapidOCR
+from rapidocr import RapidOCR
 
-engine = RapidOCR(
-    params={
-        "Rec.ocr_version": OCRVersion.PPOCRV5,
-        "Rec.engine_type": EngineType.PADDLE,
-        "Rec.model_type": ModelType.MOBILE,
-    }
-)
+engine = RapidOCR()
 
 img_url = "https://img1.baidu.com/it/u=3619974146,1266987475&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=516"
 result = engine(img_url)
