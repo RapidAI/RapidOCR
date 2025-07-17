ba9ad22cf67d7929203b377e370171ce174590dc
SWHL <liekkaskono@163.com>
2025-07-17 08:29:12 +0800

    chore(rapidocr): optim config code of openvino

--- python/rapidocr/inference_engine/openvino.py
+++ python/rapidocr/inference_engine/openvino.py
@@ -4,6 +4,7 @@
 import os
 import traceback
 from pathlib import Path
+from typing import Any, Dict
 
 import numpy as np
 from omegaconf import DictConfig
@@ -45,43 +46,47 @@ class OpenVINOInferSession(InferSession):
         model_path = Path(model_path)
         self._verify_model(model_path)
 
-        cpu_nums = os.cpu_count()
-        config = {}
-        engine_cfg = cfg.get("engine_cfg", {})
-        infer_num_threads = engine_cfg.get("inference_num_threads", -1)
-        if infer_num_threads != -1 and 1 <= infer_num_threads <= cpu_nums:
-            config["INFERENCE_NUM_THREADS"] = str(infer_num_threads)
-            
-        performance_hint = engine_cfg.get("performance_hint", None)
-        if performance_hint is not None:
-            config["PERFORMANCE_HINT"] = str(performance_hint)
-            
-        performance_num_requests = engine_cfg.get("performance_num_requests", -1)
-        if performance_num_requests != -1:
-            config["PERFORMANCE_HINT_NUM_REQUESTS"] = str(performance_num_requests)
-            
-        enable_cpu_pinning = engine_cfg.get("enable_cpu_pinning", None)
-        if enable_cpu_pinning is not None:
-            config["ENABLE_CPU_PINNING"] = str(enable_cpu_pinning)
-            
-        num_streams = engine_cfg.get("num_streams", -1)
-        if num_streams != -1:
-            config["NUM_STREAMS"] = str(num_streams)
-            
-        enable_hyper_threading = engine_cfg.get("enable_hyper_threading", None)
-        if enable_hyper_threading is not None:
-            config["ENABLE_HYPER_THREADING"] = str(enable_hyper_threading)
-        
-        scheduling_core_type = engine_cfg.get("scheduling_core_type", None)
-        if scheduling_core_type is not None:
-            config["SCHEDULING_CORE_TYPE"] = str(scheduling_core_type)
-        
-        self.logger.info(f"Using OpenVINO config: {config}")
+        config = self._init_config(cfg)
         core.set_property("CPU", config)
 
         model_onnx = core.read_model(model_path)
         compile_model = core.compile_model(model=model_onnx, device_name="CPU")
         self.session = compile_model.create_infer_request()
+
+    def _init_config(self, cfg: DictConfig) -> Dict[Any, Any]:
+        config = {}
+        engine_cfg = cfg.get("engine_cfg", {})
+
+        infer_num_threads = engine_cfg.get("inference_num_threads", -1)
+        if infer_num_threads != -1 and 1 <= infer_num_threads <= os.cpu_count():
+            config["INFERENCE_NUM_THREADS"] = str(infer_num_threads)
+
+        performance_hint = engine_cfg.get("performance_hint", None)
+        if performance_hint is not None:
+            config["PERFORMANCE_HINT"] = str(performance_hint)
+
+        performance_num_requests = engine_cfg.get("performance_num_requests", -1)
+        if performance_num_requests != -1:
+            config["PERFORMANCE_HINT_NUM_REQUESTS"] = str(performance_num_requests)
+
+        enable_cpu_pinning = engine_cfg.get("enable_cpu_pinning", None)
+        if enable_cpu_pinning is not None:
+            config["ENABLE_CPU_PINNING"] = str(enable_cpu_pinning)
+
+        num_streams = engine_cfg.get("num_streams", -1)
+        if num_streams != -1:
+            config["NUM_STREAMS"] = str(num_streams)
+
+        enable_hyper_threading = engine_cfg.get("enable_hyper_threading", None)
+        if enable_hyper_threading is not None:
+            config["ENABLE_HYPER_THREADING"] = str(enable_hyper_threading)
+
+        scheduling_core_type = engine_cfg.get("scheduling_core_type", None)
+        if scheduling_core_type is not None:
+            config["SCHEDULING_CORE_TYPE"] = str(scheduling_core_type)
+
+        self.logger.info(f"Using OpenVINO config: {config}")
+        return config
 
     def __call__(self, input_content: np.ndarray) -> np.ndarray:
         try:
