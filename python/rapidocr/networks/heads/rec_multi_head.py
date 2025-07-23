f33913dc9f242136b0b91eb68d0c2bae1051b063
SWHL <liekkaskono@163.com>
2025-07-23 09:36:57 +0800

    chore: update files

--- python/rapidocr/networks/heads/rec_multi_head.py
+++ python/rapidocr/networks/heads/rec_multi_head.py
@@ -1,7 +1,6 @@
-from torch import nn
+import torch.nn as nn
 
 from ..necks.rnn import Im2Seq, SequenceEncoder
-from .rec_ctc_head import CTCHead
 
 
 class FCTranspose(nn.Module):
@@ -14,8 +13,7 @@ class FCTranspose(nn.Module):
     def forward(self, x):
         if self.only_transpose:
             return x.permute([0, 2, 1])
-        else:
-            return self.fc(x.permute([0, 2, 1]))
+        return self.fc(x.permute([0, 2, 1]))
 
 
 class MultiHead(nn.Module):
@@ -29,7 +27,6 @@ class MultiHead(nn.Module):
             name = list(head_name)[0]
             if name == "SARHead":
                 pass
-
             elif name == "NRTRHead":
                 pass
             elif name == "CTCHead":
@@ -44,15 +41,30 @@ class MultiHead(nn.Module):
                 head_args = self.head_list[idx][name].get("Head", {})
                 if head_args is None:
                     head_args = {}
-
-                self.ctc_head = CTCHead(
+                self.ctc_head = eval(name)(
                     in_channels=self.ctc_encoder.out_channels,
                     out_channels=out_channels_list["CTCLabelDecode"],
                     **head_args,
                 )
             else:
-                raise NotImplementedError(f"{name} is not supported in MultiHead yet")
+                raise NotImplementedError(
+                    "{} is not supported in MultiHead yet".format(name)
+                )
 
     def forward(self, x, data=None):
         ctc_encoder = self.ctc_encoder(x)
-        return self.ctc_head(ctc_encoder)
+        ctc_out = self.ctc_head(ctc_encoder)
+        head_out = dict()
+        head_out["ctc"] = ctc_out
+        head_out["res"] = ctc_out
+        head_out["ctc_neck"] = ctc_encoder
+        # eval mode
+        if not self.training:
+            return ctc_out
+        if self.gtc_head == "sar":
+            sar_out = self.sar_head(x, data[1:])["res"]
+            head_out["sar"] = sar_out
+        else:
+            gtc_out = self.gtc_head(self.before_gtc(x), data[1:])["res"]
+            head_out["nrtr"] = gtc_out
+        return head_out
