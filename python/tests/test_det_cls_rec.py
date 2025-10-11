c75732395ed957a5430b9f9d0b356f9dbde6cc29
SWHL <liekkaskono@163.com>
2025-10-11 21:23:15 +0800

    test: update the image of the test cls

--- python/tests/test_det_cls_rec.py
+++ python/tests/test_det_cls_rec.py
@@ -31,7 +31,7 @@ def test_only_cls(engine):
     img_path = tests_dir / "text_cls.jpg"
     result = engine(img_path, use_det=False, use_cls=True, use_rec=False)
     assert len(result) == 1
-    assert result.cls_res[0][0] == "0"
+    assert result.cls_res[0][0] == "180"
 
 
 def test_only_rec(engine):
