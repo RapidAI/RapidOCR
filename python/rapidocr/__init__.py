802c05647f705cb225b637533d303e137fa38922
SWHL <liekkaskono@163.com>
2026-07-08 16:04:02 +0800

    fix: fixed issue #709

--- python/rapidocr/__init__.py
+++ python/rapidocr/__init__.py
@@ -1,11 +1,32 @@
 # -*- encoding: utf-8 -*-
 # @Author: SWHL
 # @Contact: liekkaskono@163.com
-from .main import RapidOCR
-from .utils.download_models import download_models
-from .utils.load_image import LoadImageError
 from .utils.typings import EngineType, LangCls, LangDet, LangRec, ModelType, OCRVersion
-from .utils.vis_res import VisRes
+
+
+def __getattr__(name):
+    if name == "RapidOCR":
+        from .main import RapidOCR
+
+        return RapidOCR
+
+    if name == "LoadImageError":
+        from .utils.load_image import LoadImageError
+
+        return LoadImageError
+
+    if name == "VisRes":
+        from .utils.vis_res import VisRes
+
+        return VisRes
+
+    if name == "download_models":
+        from .utils.download_models import download_models
+
+        return download_models
+
+    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
+
 
 __all__ = [
     "RapidOCR",
